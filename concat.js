2541652e86ddcbfc899d6863d96f81d6fd918b9f
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-30 23:59:24 +0600

    practice and learn about concat and includes in string

---
+++ concat.js
@@ -0,0 +1,20 @@
+
+// 2 ta string ke join dewar jonno amra (+) sign use korte pari
+
+/* const first = 'Tofayel';
+const last = 'Ahmed';
+const fullName = first + ' ' + last;
+console.log(fullName); */
+
+
+/* ---another way to join string----By using concat()-- */
+const first = 'Tofayel';
+const last = 'Ahmed';
+const fullName = first.concat(' ').concat(last);
+// console.log(fullName);
+
+
+
+/* ------includes----- */
+/* ---includes use kora hoy string er value er modhe search korar jonno----khuje pele output true na pele false */
+console.log(last.includes('e'));
