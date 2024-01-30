6dbd58d22d3c8e547540226830c199b27e44b084
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-31 01:20:16 +0600

    practice and learn about how to declare object

---
+++ object.js
@@ -0,0 +1,23 @@
+
+/* ----how to declare a object---- */
+
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+
+// object er moddhe er variable er value gulor man number , string, float, integer, array o hoite pare abar object o hoite pare
+
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda mam',
+    examDate: '30 feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'final exam',
+        marks: 100
+    }
+}
