0da83789ef1c1e44812f2e1669b6c60a550803ef
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-31 14:19:42 +0600

    learn how many ways we can declare a object in js

---
+++ declare.js
@@ -0,0 +1,10 @@
+
+const pen = { brand: 'echono', price: 5, color: 'black'}
+
+/* --another way to object declare--- */
+const pencil =  new Object();//emty object but amra chaile value diye dite pari
+// console.log(pencil);
+
+/* ---another way to declare object--- */
+const rubber = Object.create({});//emty object but amra chaile value diye dite pari
+console.log(rubber);
