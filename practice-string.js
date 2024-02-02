68dd3161c618d00de276b2693eb205c7687d62a1
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-02 16:05:17 +0600

    finished practice task about string task five

--- practice-string.js
+++ practice-string.js
@@ -1,6 +1,6 @@
 
 /* --practice task one----- */
-
+/* question: Count how many times a string has the letter a */
 /* let name = 'aabuaakul';
 let count = name.split('a').length-1;
 console.log(count); */
@@ -8,6 +8,7 @@ console.log(count); */
 
 
 /* ----practice task two */
+/* question: Count how many times a string has the letter a or A */
 /* let name = 'aabuaakulAA';
 let count = 0;
 let count2 = 0;
@@ -29,6 +30,8 @@ console.log('CapitalLetter:', count2); */
 
 
 /* ----practice task four---- */
+/* question:If a given string has either x, replace x by y. if the given string has X, replace it by Y. */
+
 /* let str = 'maximum';
 let str2 = 'MAX';
 let replace = str.replace('x', 'y');
@@ -38,6 +41,30 @@ console.log(replace2); */
 
 
 
+
+/* -----practice task five---- */
+/* question: Capitalize Every first Letter of each word in a String */
+/* let sentence = 'i am a goOD learnEr';
+console.log(sentence);
+let words = sentence.split(' ');
+
+for(let i = 0; i < words.length; i++){
+    words[i] = words[i] [0].toLocaleUpperCase() + words[i].toLocaleLowerCase().slice(1);
+}
+let converted = words.join(' ');
+console.log(converted);
+ */
+
+
+/* let sentence = "i aM a seRioUs leArneR";
+console.log(sentence);
+let convertArray = sentence.split(' ');
+
+for(let i = 0; i< convertArray.length; i++){
+    convertArray[i]= convertArray[i] [0].toUpperCase()+ convertArray[i].slice(1).toLowerCase();
+}
+convertStr = convertArray.join(' ');
+console.log(convertStr); */
 
 
 
